e5f820fbcf987af22cb69c9cd43131c5dc0d5613
deviaze <dev@deviaze.com>
2024-06-08 17:09:58 -0500

    added prelim windows support

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 extern crate libc;
 use std::ffi::CString;
+use std::env;
 
 fn main() {
     unsafe {
         let system = libc::system;
-        let cclr = CString::new("clear").unwrap();
+        let clear_string = match env::consts::OS {
+            "windows" => "cls",
+            _ => "clear"
+        };
+        let cclr = CString::new(clear_string).unwrap();
         system(cclr.as_ptr() as *const u8);
     }
 }
